d5862227c8a29d444d0a7f9c2c2a52d2dd3e496a
andrepatta <andre.patta@electroneum.com>
2023-08-07 00:09:49 -0300

    remove harpoon and add undotree to which-key

--- lua/andrepatta/plugins/which-key.lua
+++ lua/andrepatta/plugins/which-key.lua
@@ -17,23 +17,6 @@ wk.mappings["s"]["w"] = {
   ' ' .. kind.cmp_kind.EnumMember .. "Search Word Under Cursor"
 }
 
-wk.mappings["m"] = {
-  name = "Harpoon",
-  m = { ":lua require('harpoon.mark').add_file()<cr>", "Mark file" },
-  t = { ":lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle UI" },
-  a = { ":lua require('harpoon.ui').nav_file(1)<cr>", "Goto mark 1" },
-  s = { ":lua require('harpoon.ui').nav_file(2)<cr>", "Goto mark 2" },
-  d = { ":lua require('harpoon.ui').nav_file(3)<cr>", "Goto mark 3" },
-  f = { ":lua require('harpoon.ui').nav_file(4)<cr>", "Goto mark 4" },
-  g = { ":lua require('harpoon.ui').nav_file(5)<cr>", "Goto mark 5" },
-  q = { ":lua require('harpoon.ui').nav_file(6)<cr>", "Goto mark 6" },
-  w = { ":lua require('harpoon.ui').nav_file(7)<cr>", "Goto mark 7" },
-  e = { ":lua require('harpoon.ui').nav_file(8)<cr>", "Goto mark 8" },
-  r = { ":lua require('harpoon.ui').nav_file(9)<cr>", "Goto mark 9" },
-  n = { ":lua require('harpoon.ui').nav_next()<cr>", "Next file" },
-  p = { ":lua require('harpoon.ui').nav_prev()<cr>", "Prev file" },
-}
-
 wk.mappings["W"] = {
   name = "Window Ctrl",
   h = { '<C-w>|', 'Maximize window horizontally (|)' },
@@ -43,6 +26,8 @@ wk.mappings["W"] = {
 }
 
 wk.mappings['x'] = { ":xa<cr>", "Save All and Quit", }
+
+wk.mappings['u'] = { "Undotree" }
 
 wk.mappings["t"] = {
   name = 'Telescope',
