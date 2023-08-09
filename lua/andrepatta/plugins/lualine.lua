225afbdce967d833a64cff88c73efe07d5a7b3a4
andrepatta <andre.patta@electroneum.com>
2023-08-09 00:50:02 -0300

    lualine update

--- lua/andrepatta/plugins/lualine.lua
+++ lua/andrepatta/plugins/lualine.lua
@@ -54,8 +54,8 @@ lvim.builtin.lualine.sections = {
 
   lualine_a = { "mode" },
   lualine_b = { "branch" },
-  lualine_c = { "filename" },
-  lualine_x = { "encoding", "fileformat", "filetype" },
+  lualine_c = { "filename", "diff" },
+  lualine_x = { "diagnostics", "filetype" },
   lualine_y = { "progress" },
   lualine_z = { "location" },
 }
